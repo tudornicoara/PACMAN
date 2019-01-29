94aca895f9872932ce2213d6db92aeb7f5063683
Claudiu19 <claudu_97@yahoo.com>
2019-01-29 14:38:25 +0200

    Add files via upload

--- interface/script.js
+++ interface/script.js
@@ -1,21 +1,30 @@
+
+
 window.onload = function(){
-    document.getElementById("start").style.display = 'none';
-    document.getElementById("login").style.display = 'flex';
+    if(localStorage.getItem("UserId")){
+        document.getElementById("start").style.display = 'flex';
+        document.getElementById("login").style.display = 'none';
+    }
+    else{
+        document.getElementById("start").style.display = 'none';
+        document.getElementById("login").style.display = 'flex';
+    }
     document.getElementById("game").style.display = 'none';
 }
 
 document.getElementById("login").addEventListener("click", function() {
     document.getElementById("login").style.display = 'none';
     var provider = new firebase.auth.FacebookAuthProvider();
-    console.log(provider)
-    console.log(firebase.auth().signInWithPopup(provider))
     firebase.auth().signInWithPopup(provider).then(function(result) {
         console.log("1")
         // This gives you a Facebook Access Token. You can use it to access the Facebook API.
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
-        console.log(token)
+        localStorage.setItem("UserId", user.uid);
+        photoURL = user.providerData[0]["photoURL"]
+        localStorage.setItem("PhotoUrl", photoURL)
+        
       }).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
